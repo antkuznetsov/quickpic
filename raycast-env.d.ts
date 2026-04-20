ac015e48e3457733c42058c47670998a6ecfa2c1
Anton Kuznetsov <antkuznetsov@yandex.ru>
2026-04-20 23:53:06 +0200

    Fix Prettier printWidth

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -7,31 +7,30 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {};
 
 /** Preferences accessible in all the extension's commands */
-declare type Preferences = ExtensionPreferences
+declare type Preferences = ExtensionPreferences;
 
 declare namespace Preferences {
   /** Preferences accessible in the `library` command */
-  export type Library = ExtensionPreferences & {}
+  export type Library = ExtensionPreferences & {};
   /** Preferences accessible in the `add-image` command */
-  export type AddImage = ExtensionPreferences & {}
+  export type AddImage = ExtensionPreferences & {};
   /** Preferences accessible in the `add-selected-image` command */
-  export type AddSelectedImage = ExtensionPreferences & {}
+  export type AddSelectedImage = ExtensionPreferences & {};
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `library` command */
-  export type Library = {}
+  export type Library = {};
   /** Arguments passed to the `add-image` command */
-  export type AddImage = {}
+  export type AddImage = {};
   /** Arguments passed to the `add-selected-image` command */
   export type AddSelectedImage = {
-  /** Keywords (comma separated) */
-  "keywords": string,
-  /** Optional title */
-  "title": string
+    /** Keywords (comma separated) */
+    keywords: string;
+    /** Optional title */
+    title: string;
+  };
 }
-}
-
